dd3ecbe12b40ce2e9e963e0b2e948e36674402f4
salman-khaann <thesmartboy993@gmail.com>
2024-03-27 18:20:24 +0500

    solved-2/45-questions

---
+++ salman.ts
@@ -0,0 +1,4 @@
+//Que no 2; 2. Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+
+const pName : string = "salmanKhan";
+console.log(`"hello ${pName} would you like to learn some typeScript today?"`);
